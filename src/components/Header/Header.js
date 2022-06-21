2e4b2d72d5f1d0c2200726e0efb79c660b335ffb
Yavuz Selim Şerifoğlu <70685127+serifselim@users.noreply.github.com>
2022-06-21 21:32:49 +0300

    add page is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,12 @@ import utilsStlyes from '../../styles/utils.module.css';
 import styles from './Header.module.css';
 // Assets
 import logoSrc from '../../assets/logo.png';
+import returnSrc from '../../assets/return.svg';
 
 const Header = () => {
     // Style Variables
     const { btn, input } = utilsStlyes;
-    const { logoBox, form, formBox, addBox, headerBox, header } = styles;
+    const { logoBox, form, formBox, addBox, headerBox, header, returnBox, returnBtn } = styles;
 
     const { pathname } = useLocation();
 
@@ -18,26 +19,40 @@ const Header = () => {
         <header className={header}>
 
             <section className={headerBox}>
+
                 <div className={logoBox}>
                     {pathname !== '/' &&
                         <img src={logoSrc} alt='logo' height={63} />
                     }
+                    {pathname === '/add' &&
+                        <div className={returnBox}>
+                            <a className={returnBtn} href='#/'>
+                                <img src={returnSrc} alt='icon' />
+                                Return to List Page
+                            </a>
+                        </div>
+                    }
                 </div>
-                <div className={formBox}>
-                    {pathname === '/result' &&
+
+
+
+                {pathname === '/result' &&
+                    <div className={formBox}>
                         <form className={form}>
                             <input placeholder='Search' className={input} required />
                             <button className={btn} >Search</button>
                         </form>
-                    }
-                </div>
-                <div className={addBox}>
-                    {pathname !== '/add' &&
+                    </div>
+                }
+
+                {pathname !== '/add' &&
+                    <div className={addBox}>
                         <button className={btn} >
                             Add new record
                         </button>
-                    }
-                </div>
+                    </div>
+                }
+
             </section>
         </header>
     );
--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,8 +1,29 @@
 import React from 'react';
+// Styles
+import styles from './Add.module.css';
+import utilsStyles from '../../styles/utils.module.css';
 
 const Add = () => {
+    // Styles Variables
+    const { container, addBox, addForm, addLabel } = styles;
+    const { input, btn } = utilsStyles;
+
     return (
-        <div>Add</div>
+        <main className={container}>
+            <div className={addBox}>
+                <form className={addForm}>
+                    <label className={addLabel}>Name Surname</label>
+                    <input required className={input} placeholder='Enter name and surname' />
+                    <label className={addLabel}>Country</label>
+                    <input required className={input} placeholder='Enter a country' />
+                    <label className={addLabel}>Name Surname</label>
+                    <input required className={input} placeholder='Enter a city' />
+                    <label className={addLabel}>Name Surname</label>
+                    <input required type='email' className={input} placeholder='Enter an e-mail(abc@xyz.com)' />
+                    <button className={btn}>Add</button>
+                </form>
+            </div>
+        </main>
     );
 };
 
